0435b1505a7b4552491f705a636e3ed1f7aed024
Paul Kim <paulhyunkim@gmail.com>
2014-06-21 15:54:24 -0700

    default map is now white, dropdown menu starts without a map loaded

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = angular.module('myApp', ['vr.directives.slider'])
   $scope.cx = "5";
   $scope.circles=[5,10,15,20,25];
   $scope.datasets = [
+  	{name:"Select MDG Goal", src:''},
   	{name:'A', src:'goal1target1aa.json'},
   	{name:'B', src:'goal6target6aa.json'}
   ];
