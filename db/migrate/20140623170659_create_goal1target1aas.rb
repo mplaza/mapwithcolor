b5dbf35c725ababe855f42fe066a22eb3976e383
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-23 18:59:28 -0700

    railsified

---
+++ db/migrate/20140623170659_create_goal1target1aas.rb
@@ -0,0 +1,32 @@
+class CreateGoal1target1aas < ActiveRecord::Migration
+  def change
+    create_table :goal1target1aas do |t|
+      t.string :CountryCode
+      t.string :Country
+      t.string   :year1990
+      t.string   :year1991
+      t.string   :year1992
+      t.string   :year1993
+      t.string   :year1994
+      t.string   :year1995
+      t.string   :year1996
+      t.string   :year1997
+      t.string   :year1998
+      t.string   :year1999
+      t.string   :year2000
+      t.string   :year2001
+      t.string   :year2002
+      t.string   :year2003
+      t.string   :year2004
+      t.string   :year2005
+      t.string   :year2006
+      t.string   :year2007
+      t.string   :year2008
+      t.string   :year2009
+      t.string   :year2010
+      t.string   :year2011
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20140623232546_change_type_of_year.rb
@@ -0,0 +1,29 @@
+class ChangeTypeOfYear < ActiveRecord::Migration
+  def change
+
+  	change_column :goal1target1aas, :year1990, 'float USING CAST(year1990 AS float)'
+  	change_column :goal1target1aas, :year1991, 'float USING CAST(year1991 AS float)'
+  	change_column :goal1target1aas, :year1992, 'float USING CAST(year1992 AS float)'
+  	change_column :goal1target1aas, :year1993, 'float USING CAST(year1993 AS float)'
+  	change_column :goal1target1aas, :year1994, 'float USING CAST(year1994 AS float)'
+  	change_column :goal1target1aas, :year1995, 'float USING CAST(year1995 AS float)'
+  	change_column :goal1target1aas, :year1995, 'float USING CAST(year1996 AS float)'
+  	change_column :goal1target1aas, :year1997, 'float USING CAST(year1997 AS float)'
+  	change_column :goal1target1aas, :year1998, 'float USING CAST(year1998 AS float)'
+  	change_column :goal1target1aas, :year1999, 'float USING CAST(year1999 AS float)'
+  	change_column :goal1target1aas, :year2000, 'float USING CAST(year2000 AS float)'
+  	change_column :goal1target1aas, :year2001, 'float USING CAST(year2001 AS float)'
+  	change_column :goal1target1aas, :year2002, 'float USING CAST(year2002 AS float)'
+  	change_column :goal1target1aas, :year2003, 'float USING CAST(year2003 AS float)'
+  	change_column :goal1target1aas, :year2004, 'float USING CAST(year2004 AS float)'
+  	change_column :goal1target1aas, :year2005, 'float USING CAST(year2005 AS float)'
+  	change_column :goal1target1aas, :year2006, 'float USING CAST(year2006 AS float)'
+  	change_column :goal1target1aas, :year2007, 'float USING CAST(year2007 AS float)'
+  	change_column :goal1target1aas, :year2008, 'float USING CAST(year2008 AS float)'
+  	change_column :goal1target1aas, :year2009, 'float USING CAST(year2009 AS float)'
+  	change_column :goal1target1aas, :year2010, 'float USING CAST(year2011 AS float)'
+  	change_column :goal1target1aas, :year2011, 'float USING CAST(year2011 AS float)'
+
+
+  end
+end
